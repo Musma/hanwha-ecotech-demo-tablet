1d2a37d64ccb94738b85ac2ac9aa1e64780d77a7
dave352 <dave@musma.net>
2026-07-10 11:15:48 +0900

    style(layout): 콘텐츠 padding을 22px로 통일

--- src/shared/components/logged-page-shell.vue
+++ src/shared/components/logged-page-shell.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex h-full flex-col bg-hw-white-lighter">
-    <div class="flex-1 px-[22px] pt-[18px] pb-[60px]">
+    <div class="flex-1 p-[22px]">
       <slot />
     </div>
   </div>
